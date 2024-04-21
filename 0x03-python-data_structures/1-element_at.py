acedcf440bdc2eadaa8c14a4d1b316e2497a68d6
karabanick <nikmunuhe@gmail.com>
2024-04-21 15:27:31 +0300

    retrieving an element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+
+    count = 0
+    for i in my_list:
+        if count == idx:
+            return i
+        count += 1
+
+    return None
